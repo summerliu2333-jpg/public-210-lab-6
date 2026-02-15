83f0a3ed98166c4e8263be11875f7f68d3ae1142
Yang Liu <summerliu2333@gmail.com>
2026-02-14 18:45:12 -0800

    Initial commit

---
+++ lab6.cpp
@@ -0,0 +1,14 @@
+//COMSC-210-5068, Lab 6,Yang Liu
+#include <iostream>
+using namespace std;
+
+
+void enterArrayData(double* arr, int size);
+void outputArrayData(double* arr, int size);
+double sumArray(double* arr, int size);
+
+int main(){
+
+    
+return 0;
+}
